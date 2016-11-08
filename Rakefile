e146da50b6648ca337f75f43c1a8b899db76f495
Sonia Boller <soniaboller@Sonias-MacBook-Pro.local>
2016-11-08 15:23:02 -0600

    good feelz

---
+++ Rakefile
@@ -0,0 +1,7 @@
+require 'sinatra/activerecord'
+require 'sinatra/activerecord/rake'
+
+ActiveRecord::Base.establish_connection(
+    :adapter => 'mysql2',
+    :database => 'superstars'
+)
--- controllers/superstar_controller.rb
+++ controllers/superstar_controller.rb
@@ -1,18 +1,65 @@
 class SuperstarController < ApplicationController
 
   get '/' do
-    {:message => "all items soon"}.to_json
+    # get all
+    Superstar.all.to_json
   end
 
-  get '/fab' do
-    # use activerecord for database
-    @christopher = Superstar.new
-    @christopher.talent = 'socialite'
-    @christopher.name = 'Christopher'
-    @christopher.outfit = 'blue sweater swag'
-    @christopher.save
-    @christopher.to_json
-    
-    # CRUD -- create, read, update, delete
+  get '/:id' do
+    # get by id
+    @id = params[:id] # grabs id from URL
+    Superstar.find(@id).to_json
   end
+
+  post '/' do
+    # create
+    @name = params[:name]
+    @talent = params[:talent]
+    @outfit = params[:outfit]
+
+    @model = Superstar.new
+    @model.name = @name
+    @model.talent = @talent
+    @model.outfit = @outfit
+    @model.save
+
+    # place a breakpoint in ruby!!
+    # binding.pry
+    # ^ code stops here
+    @model.to_json
+  end
+
+  patch '/:id' do
+    # update
+    @id = params[:id]
+    @name = params[:name]
+    @talent = params[:talent]
+    @outfit = params[:outfit]
+    @model = Superstar.find(@id)
+    @model.name = @name
+    @model.talent = @talent
+    @model.outfit = @outfit
+    @model.save
+    @model.to_json
+  end
+
+  delete '/:id' do
+    # delete
+    @id = params[:id]
+    @model = Superstar.find(@id)
+    @model.destroy
+    {:message => "item of id " + @id + " was deleted"}.to_json
+  end
+
+  # get '/fab' do
+  #   # use activerecord for database
+  #   @christopher = Superstar.new
+  #   @christopher.talent = 'socialite'
+  #   @christopher.name = 'Christopher'
+  #   @christopher.outfit = 'blue sweater swag'
+  #   @christopher.save
+  #   @christopher.to_json
+  #
+  #   # CRUD -- create, read, update, delete
+  # end
 end
